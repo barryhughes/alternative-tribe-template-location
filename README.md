8d4635f6da5e6857a7bd3b946893196c6a1016d2
barryhughes <barry@codingkills.me>
2017-08-02 10:07:18 -0700

    Initial work on alternative template override support

---
+++ README.md
@@ -0,0 +1,11 @@
+## Alternative Tribe Template Location
+
+By default, The Events Calendar, Event Tickets and family support placing template overrides within a `tribe-events` sub-folder of the current theme.
+
+That isn't always ideal, though, so this plugin adds support for a further location: `wp-content/tribe-events`.
+
+### Usage
+
+* Install and activate like any other plugin
+* Locate any template overrides for The Events Calendar/associated plugins in a new `wp-content/tribe-events` directory
+* Enjoy trouble free updates of your theme (parent and child themes alike) without fear of losing calendar customizations
---
+++ alternative-tribe-template-location.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Plugin name: Alternative Tribe Template Location
+ * Description: Placing template overrides in a theme or even a child theme is not always optimal. This plugin adds support for locating template overrides within a wp-content/tribe-events directory. Useful for The Events Calendar, Event Tickets and related plugins.
+ * Author:      Barry Hughes
+ * Author URI:  https://codingkills.me
+ * License:     GPLv3
+ */
+
+class Alternative_Tribe_Template_Location_Facilitator {
+	protected $alternative_path = '';
+
+	public function __construct() {
+		if ( $this->alternative_path() ) {
+			add_filter( 'tribe_events_template', array( $this, 'filter_template_paths' ) );
+		}
+	}
+
+	protected function alternative_path() {
+		$path = trailingslashit( WP_CONTENT_DIR ) . 'tribe-events';
+
+		if ( is_dir( $path ) && is_readable( $path ) ) {
+			$this->alternative_path = $path;
+			return true;
+		}
+
+		return false;
+	}
+
+	public function filter_template_paths( $path ) {
+		// We expect the templat path to contain a /src/views/ component
+		$src_views = strpos( $path, '/src/views/' );
+
+		if ( ! $src_views ) {
+			return $path;
+		}
+
+		$sub_path = substr( $path, $src_views + 11 );
+		$possible_override = $this->alternative_path . '/' . $sub_path;
+
+		if ( file_exists( $possible_override ) ) {
+			return $possible_override;
+		}
+
+		return $path;
+	}
+}
+
+function alt_tribe_template_location_facilitator() {
+	static $object;
+
+	return empty( $object )
+		? $object = new Alternative_Tribe_Template_Location_Facilitator
+		: $object;
+}
+
+alt_tribe_template_location_facilitator();
